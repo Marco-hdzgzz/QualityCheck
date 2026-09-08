8a3fa1350d8dbb70a893b1f3619ea1dd8bf7ce0d
Carmelita Santos <carmelitarociosantoscastillo@gmail.com>
2026-09-08 15:35:12 -0600

    pantalla login

---
+++ cadena.py
@@ -0,0 +1,14 @@
+phrase = "Giraffe Academy"
+#print(phrase + "is cool")
+#print(phrase.lower())
+#print(phrase.upper())
+
+#print(phrase.upper().isupper())
+#print(len(phrase))
+
+#print(phrase[0])
+#print(phrase[6])
+
+#print(phrase.index("A"))
+
+print(phrase.replace("Giraffe", "Butterfly"))
